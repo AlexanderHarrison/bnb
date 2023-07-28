df5d0af18cb45e8c7c162fc92e5fee8a654227bd
Alex Harrison <alexander.harrison.r@gmail.com>
2023-07-28 13:22:27 -0600

    license, readme, doc changes

--- kilp.py
+++ kilp.py
@@ -12,10 +12,10 @@ def solve(c, A, k, log=True):
     """Solve for the `k` best integer solutions to the linear optimization problem of the form:
 
     minimize dot(c, x)
-    subject to A @ x <= 1.0
-    and x >= 0
+    subject to A @ x <= 1
+    and 0 <= x <= 1
  
-    `c` -- const vector as a 1D numpy array.
+    `c` -- cost vector as a 1D numpy array.
     `A` -- constraint matrix as a 2D numpy matrix or a sparse scipy matrix. Width must be the same as c.
     `k` -- number of solutions to find.
 
